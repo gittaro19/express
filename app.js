e569934ab9ad824550a9edc0f632d67d9714066c
gittaro19 <neverending.voyage@gmail.com>
2025-02-01 06:17:50 +0900

    Add Page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var helloRouter = require('./routes/hello');
+var originalRouter = require('./routes/original');
 var notesRouter = require('./routes/notes');
 
 var app = express();
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/hello', helloRouter);
+app.use('/original', originalRouter);
 app.use('/notes', notesRouter);
 
 // catch 404 and forward to error handler
--- routes/original.js
+++ routes/original.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('hello', { title: 'hello' });
+  res.render('original', { title: 'original' });
 });
 
 module.exports = router;
